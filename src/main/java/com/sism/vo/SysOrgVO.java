974743e8b391d76902c2b3382e9e52d8a7eebb1f
muzimu217 <muzimu217@users.noreply.github.com>
2026-02-16 06:52:17 -0800

    fix: add JsonProperty annotations to SysOrgVO for field name mapping

--- src/main/java/com/sism/vo/SysOrgVO.java
+++ src/main/java/com/sism/vo/SysOrgVO.java
@@ -1,5 +1,6 @@
 package com.sism.vo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sism.enums.OrgType;
 import lombok.Data;
 
@@ -14,11 +15,13 @@ public class SysOrgVO {
     /**
      * Organization ID
      */
+    @JsonProperty("orgId")
     private Long id;
 
     /**
      * Organization name (unique)
      */
+    @JsonProperty("orgName")
     private String name;
 
     /**
@@ -27,6 +30,7 @@ public class SysOrgVO {
      * FUNCTIONAL_DEPT - Functional Department (Mid-level Management)
      * COLLEGE - Secondary College (Execution Layer)
      */
+    @JsonProperty("orgType")
     private OrgType type;
 
     /**
